dc5facc0dff512516efbe0cd96dc603d9142b72c
Marko Rusic <markousp5@gmail.com>
2018-10-01 22:28:00 +0200

    test boilerplate; intent rules fixed

--- src/templates/component.js
+++ src/templates/component.js
@@ -2,25 +2,24 @@ const rcfp = ({ componentName }) => `import React from 'react'
 import PropTypes from 'prop-types'
 
 const ${componentName} = ({ children }) => (
-  <div>
+	<div>
 		{children}
 	</div>
 )
 
 ${componentName}.propTypes = {
-  children: PropTypes.any
+	children: PropTypes.any
 }
 
 export default ${componentName}
-
 `
 
 const index = ({ componentName }) => `import ${componentName} from './${componentName}'
 import styles from './styles'
 
 export {
-  ${componentName},
-  styles
+	${componentName},
+	styles
 }
 `
 
@@ -29,7 +28,16 @@ const styles = () => `export default {
 }
 `
 
-const test = ({ componentName }) => `
+const test = ({ componentName }) => `/* eslint-disable */
+import React from 'react'
+import ReactDOM from 'react-dom'
+import ${componentName} from './${componentName}'
+
+it('${componentName} renders without crashing', () => {
+	const div = document.createElement('div')
+	ReactDOM.render(<${componentName} />, div)
+	ReactDOM.unmountComponentAtNode(div)
+})
 `
 
 module.exports = {
